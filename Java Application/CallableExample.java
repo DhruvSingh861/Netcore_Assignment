58e1ea18bbfe0d2397b7d97885eadd1ff6739e5a
DhruvSingh861 <singhdhruv861@gmail.com>
2024-06-10 17:01:43 +0530

    Implemented Thread using Callable Functional Interface

---
+++ Java Application/CallableExample.java
@@ -0,0 +1,27 @@
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
+
+public class CallableExample {
+    public static void main(String[] args) {
+        CallableThread ct=new CallableThread();
+        FutureTask message=new FutureTask(ct);
+        Thread th=new Thread(message);
+        th.start();
+        try{
+            Object o=message.get();
+            System.out.println(o);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
+class CallableThread implements Callable{
+
+    @Override
+    public Object call() throws Exception {
+        System.out.println("Thread Started");
+        Thread.sleep(2000);
+        return "Thread Ended";
+    }
+}
